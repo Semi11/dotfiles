f73ac898b503dff9bb39954da4a6dffd08133be9
semi11 <stand.say.me@gmail.com>
2022-05-17 19:08:15 +0900

    fix shellscript

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+mkdir ~/.vim
 ln -sf ~/dotfiles/colors ~/.vim
 ln -sf ~/dotfiles/autoload ~/.vim
 ln -sf ~/dotfiles/.vimcheatsheet.md ~/.vimcheatsheet.md
